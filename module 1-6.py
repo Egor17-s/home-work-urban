e486217d5b43f5b0875f7dd5b907440081c89188
Egor17-s <egoralll18@gmail.com>
2024-08-13 20:47:02 +0700

    Add files via upload

---
+++ module 1-6.py
@@ -0,0 +1,16 @@
+my_dict = {'Egor': 1997, 'Nikita': 1998, 'Artem': 2000}
+print(my_dict)
+print(my_dict['Nikita'])
+print(my_dict.get('Anton', 'Таких тут нет'))
+my_dict.update({'Kris': 1994,
+                'Roma': 1996})
+print(my_dict)
+a = my_dict.pop('Roma')
+print(a)
+
+my_set = {1, 2, 3, 4, 1, 2, 4, 'Egor', 'Kto-to', True, 'Kto-to'}
+print(my_set)
+my_set.add('Tanya')
+my_set.add(5)
+my_set.discard(1)
+print(my_set)
